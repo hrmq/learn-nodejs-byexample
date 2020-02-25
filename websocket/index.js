a474bd679b476e9185dc94930d9b495245f70e65
hrmq <h.rahimi.moqadam@gmail.com>
2020-02-25 13:03:35 -0500

    add native websecoket sample with nodejs and ws module

---
+++ websocket/index.js
@@ -0,0 +1,13 @@
+const WebSocketServer = require('ws').Server;
+const wss = new WebSocketServer({ port: 3000 });
+
+wss.on('connection', ws => {
+  ws.send('XYX');
+  setInterval(() => {
+    ws.send(new Date().toLocaleTimeString());
+  }, 1000);
+
+  ws.on('message', message => {
+    console.log('received %s', message);
+  });
+});
